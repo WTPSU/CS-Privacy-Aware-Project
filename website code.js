ffc10752fbf040ade2e4fe0ceae2d62d52a6b62f
wesT <westleyt@pdx.edu>
2025-06-03 22:14:45 -0700

    Added feedback form hopefully it works?

--- website code.js
+++ website code.js
@@ -3,3 +3,26 @@ const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
 tooltipTriggerList.forEach((el) => new bootstrap.Tooltip(el));
+
+//feedback forms
+let form = document.querySelector("#feedback");
+
+form.addEventListener("submit", (e) => {
+  const form = document.querySelector("#feedback");
+  const email = form.querySelector("#email");
+  const name = form.querySelector("#name");
+  const feedback = form.querySelector("#feedbacktext");
+
+  console.log(email.value, name.value, feedback.value);
+
+  const webhookURL =
+    "https://discord.com/api/webhooks/1379688580699521175/NeAH5oqHPAN2uTbD2tdLydE90PRsLh4GYtAeEta50CSyKVWqAmwpiVgzacNqOr_yYHBI";
+  fetch(webhookURL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ content: `TEST FEEDBACK: ${feedback}` }),
+  })
+    .then(() => alert("Feedback sent!"))
+    .catch((err) => alert("Error sending feedback."));
+});
+//
